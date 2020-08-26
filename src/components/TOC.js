1f72381710132c5813e35532e6b72819e7699e7f
bomeekim <thespringg@naver.com>
2020-08-26 18:31:10 +0900

    State와 Key 사용

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,19 +3,23 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    var list = [];
+    var data = this.props.data;
+    var i = 0;
+
+    while (i < data.length) {
+      list.push(
+        // 반복문을 작성할 때는 key 값이 필요하다.
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+        </li>
+      );
+      i = i + 1;
+    }
+
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{list}</ul>
       </nav>
     );
   }
