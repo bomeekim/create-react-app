c4ddc37da5e06d886926a1605847316ab5aab025
bomeekim <thespringg@naver.com>
2020-09-01 15:42:04 +0900

    Delete 구현

--- src/App.js
+++ src/App.js
@@ -143,9 +143,30 @@ class App extends Component {
           data={this.state.contents}></TOC>
         <Control
           onChangeMode={function (_mode) {
-            this.setState({
-              mode: _mode
-            });
+            switch (_mode) {
+              case 'delete':
+                if (window.confirm('really?')) {
+                  var _contents = Array.from(this.state.contents);
+                  var itemIndex = _contents.findIndex(
+                    (o) => o.id === this.state.selected_content_id
+                  );
+
+                  _contents.splice(itemIndex, 1);
+
+                  this.setState({
+                    mode: 'welcome',
+                    contents: _contents
+                  });
+
+                  alert('deleted!');
+                }
+                break;
+              default:
+                this.setState({
+                  mode: _mode
+                });
+                break;
+            }
           }.bind(this)}></Control>
         {this.getContent()}
       </div>
